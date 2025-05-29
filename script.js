157a22179e0f5cdb5c4be7cc04f91652f6b07ce1
ninizedginidze <nino.zedginidze.1@iliauni.edu.ge>
2025-05-29 18:04:23 +0400

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
-let input = prompt("ჩაწერეთ რიცხვი:");
+let input = prompt('ჩაწერეთ რიცხვი:');
 
-if (input === null || input.trim() === "") {
-    alert("რიცხვი არ ჩაწერილა");
+if (input === null || input.trim() === '') {
+    alert('რიცხვი არ ჩაწერილა');
 } else {
     let number = Number(input);
 
     if (isNaN(number)) {
-        alert("გთხოვთ ჩაწერეთ ვალიდური რიცხვი!");
+        alert('გთხოვთ ჩაწერეთ ვალიდური რიცხვი!');
     } else if (number % 2 === 0) {
-        alert("რიცხვი ლუწია");
+        alert('რიცხვი ლუწია');
     } else {
-        alert("რიცხვი კენტია");
+        alert('რიცხვი კენტია');
     }
 }
 
